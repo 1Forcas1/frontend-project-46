101392ef0c8b764fb05f4bb72e7ed5762a5f7437
Danil Sichinava <danya.sichinava@mail.ru>
2023-03-26 22:14:08 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,6 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
