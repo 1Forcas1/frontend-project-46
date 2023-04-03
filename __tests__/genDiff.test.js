f809b9bb6aa229fa1ecc8ec5a0366351e8e32598
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-03 17:57:36 +0300

    update genDiff.test.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
+import { readFileSync } from 'node:fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,11 +9,13 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
 test('dif', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
 
-  const dif = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  const dif = readFile('expected_file.txt');
 
   expect(genDiff(file1, file2)).toEqual(dif);
 });
