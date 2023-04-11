43611b947f89f30a29636d02ae466c65370a10a5
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-11 19:32:52 +0300

    upgrade __tests__/genDiff.test.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -11,11 +11,51 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('dif', () => {
+test('dif json files', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
 
   const dif = readFile('expected_file.txt');
 
   expect(genDiff(file1, file2)).toEqual(dif);
+});
+
+test('dif json files2', () => {
+  const file3 = getFixturePath('file3.json');
+  const file4 = getFixturePath('file4.json');
+
+  const dif2 = readFile('expected_file2.txt');
+
+  expect(genDiff(file3, file4)).toEqual(dif2);
+});
+
+test('dif yml files', () => {
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yaml');
+
+  const dif = readFile('expected_file.txt');
+
+  expect(genDiff(file1, file2)).toEqual(dif);
+});
+
+test('dif plain format json files', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+
+  const dif = readFile('expected_file_plain_format.txt');
+  const formatName = 'plain';
+
+  expect(genDiff(file1, file2, formatName)).toEqual(dif);
+});
+
+test('error', () => {
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yaml');
+
+  const formatName = 'null';
+
+  const checkParse = () => genDiff(file1, file2, formatName);
+  const error = new Error(`Формат не поддерживается: ${formatName}`);
+
+  expect(checkParse).toThrow(error);
 });
