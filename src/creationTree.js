d12bd375c2c59be3e8b5e6fe57c62de2c8673273
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-11 20:00:01 +0300

    upgrade src/creationTree.js

--- src/creationTree.js
+++ src/creationTree.js
@@ -5,6 +5,8 @@ const getKeys = (obj1, obj2) => {
   return _.sortBy(keys);
 };
 
+const isObject = (obj) => (_.isObject(obj) && !Array.isArray(obj));
+
 const creationTree = (file1, file2) => {
   const fileKeys = getKeys(file1, file2);
 
@@ -13,8 +15,7 @@ const creationTree = (file1, file2) => {
       return { name: key, value: file1[key], status: 'removed' };
     }
     if (_.has(file1, key) && _.has(file2, key)) {
-      if (_.isObject(file1[key]) && _.isObject(file2[key])
-      && !Array.isArray(file1[key]) && !Array.isArray(file2[key])) {
+      if (isObject(file1[key]) && isObject(file2[key])) {
         const children = creationTree(file1[key], file2[key]);
         return { name: key, children };
       }
