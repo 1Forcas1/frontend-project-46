656fe5d02abaeb7ae7a5a9fbbccb64da1dd6a61d
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-11 19:36:24 +0300

    add src/formatters

---
+++ src/formatters/plain.js
@@ -0,0 +1,64 @@
+import _ from 'lodash';
+
+const getValueByItsType = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  } if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const getPlainDiff = (node) => {
+  if (Array.isArray(node)) {
+    const [before, after] = node;
+    return `Property '${after.name}' was updated. From ${getValueByItsType(before.value)} to ${getValueByItsType(after.value)}`;
+  }
+
+  if (_.isObject(node.value) && node.status === 'added') {
+    return `Property '${node.name}' was added with value: ${getValueByItsType(node.value)}`;
+  }
+
+  if (node.status === 'added') {
+    return `Property '${node.name}' was added with value: ${getValueByItsType(node.value)}`;
+  }
+  if (node.status === 'updated') {
+    return '';
+  }
+  return `Property '${node.name}' was removed`;
+};
+
+const plain = (tree) => {
+  const cloneTree = _.cloneDeep(tree);
+
+  const iter = (node) => {
+    const result = node.map((unit) => {
+      const { name } = unit;
+
+      if (Object.keys(unit).includes('children')) {
+        const { children } = unit;
+        const nestedProperties = children.map((child) => {
+          if (Array.isArray(child)) {
+            child.map((grandson) => {
+              // eslint-disable-next-line no-param-reassign
+              grandson.name = `${name}.${grandson.name}`;
+              return grandson;
+            });
+          }
+          // eslint-disable-next-line no-param-reassign
+          child.name = `${name}.${child.name}`;
+          return child;
+        });
+        return iter(nestedProperties);
+      }
+
+      return getPlainDiff(unit);
+    });
+
+    return _.compact(result).join('\n');
+  };
+
+  return iter(cloneTree);
+};
+
+export default plain;
---
+++ src/formatters/stylish.js
@@ -0,0 +1,67 @@
+import _ from 'lodash';
+
+const stylish = (tree, replacer = ' ', spacesCount = 4) => {
+  const iter = (node, depth) => {
+    const shiftToLeft = 2;
+    const indentSize = (spacesCount * depth) - shiftToLeft;
+    const currentIndent = replacer.repeat(indentSize);
+
+    if (_.isObject(node) && !Array.isArray(node)) {
+      const keysAndValues = Object.entries(node);
+      return keysAndValues.map(([key, value]) => {
+        if (_.isObject(value)) {
+          return `${currentIndent}  ${key}: {\n${iter(value, depth + 1)}${currentIndent}  }\n`;
+        }
+        return `${currentIndent}  ${key}: ${value}\n`;
+      }).join('');
+    }
+
+    const result = node.map((unit) => {
+      const { name } = unit;
+
+      if (Object.keys(unit).includes('children')) {
+        const { children } = unit;
+        return `${currentIndent}  ${name}: {\n${iter(children, depth + 1)}\n${currentIndent}  }`;
+      }
+      if (Array.isArray(unit)) {
+        const nearProperties = unit.map((joint) => {
+          if (_.isObject(joint.value) && joint.status === 'removed') {
+            return `${currentIndent}- ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }\n`;
+          }
+          if (_.isObject(joint.value) && joint.status === 'added') {
+            return `${currentIndent}+ ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }`;
+          }
+          if (joint.status === 'removed') {
+            return `${currentIndent}- ${joint.name}: ${joint.value}\n`;
+          }
+          return `${currentIndent}+ ${joint.name}: ${joint.value}`;
+        });
+
+        return nearProperties.join('');
+      }
+
+      if (_.isObject(unit.value) && !Array.isArray(unit.value)) {
+        if (unit.status === 'added') {
+          return `${currentIndent}+ ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
+        }
+        if (unit.status === 'removed') {
+          return `${currentIndent}- ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
+        }
+      }
+
+      if (unit.status === 'added') {
+        return `${currentIndent}+ ${name}: ${unit.value}`;
+      }
+      if (unit.status === 'removed') {
+        return `${currentIndent}- ${name}: ${unit.value}`;
+      }
+      return `${currentIndent}  ${name}: ${unit.value}`;
+    });
+
+    return result.join('\n');
+  };
+
+  return `{\n${iter(tree, 1)}\n}`;
+};
+
+export default stylish;
