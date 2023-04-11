789906214e0cbcc958c1deb00c33d5c345c14766
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-11 19:33:57 +0300

    upgrade src/index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import path from 'path';
 import { readFileSync } from 'node:fs';
-import makingDiff from './makingDiff.js';
+import creationTree from './creationTree.js';
+import parsers from './parsers.js';
+import format from './formatters/index.js';
 
 const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 
+const getExtname = (filepath) => path.extname(filepath);
+
 const readFile = (filepath) => readFileSync(filepath);
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, formatName = 'stylish') => {
   const filePath1 = getFilePath(file1);
-  const data1 = JSON.parse(readFile(filePath1));
+  const extnameFile1 = getExtname(filePath1);
+  const data1 = parsers(readFile(filePath1), extnameFile1);
 
   const filePath2 = getFilePath(file2);
-  const data2 = JSON.parse(readFile(filePath2));
+  const extnameFile2 = getExtname(filePath2);
+  const data2 = parsers(readFile(filePath2), extnameFile2);
 
-  return makingDiff(data1, data2);
+  const innerTree = creationTree(data1, data2);
+  return format(innerTree, formatName);
 };
 
 export default genDiff;
