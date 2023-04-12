61a2267f694faae80a61546a890d6412ddac3df0
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-12 20:30:00 +0300

    upgrade src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,41 +10,32 @@ const getValueByItsType = (value) => {
 };
 
 const getPlainDiff = (node) => {
-  if (Array.isArray(node)) {
-    const [before, after] = node;
-    return `Property '${after.name}' was updated. From ${getValueByItsType(before.value)} to ${getValueByItsType(after.value)}`;
-  }
-
   if (_.isObject(node.value) && node.status === 'added') {
     return `Property '${node.name}' was added with value: ${getValueByItsType(node.value)}`;
   }
 
-  if (node.status === 'added') {
-    return `Property '${node.name}' was added with value: ${getValueByItsType(node.value)}`;
+  switch (node.status) {
+    case 'added':
+      return `Property '${node.name}' was added with value: ${getValueByItsType(node.value)}`;
+    case 'updated':
+      return `Property '${node.name}' was updated. From ${getValueByItsType(node.oldValue)} to ${getValueByItsType(node.newValue)}`;
+    case 'unupdated':
+      return '';
+    default:
+      return `Property '${node.name}' was removed`;
   }
-  if (node.status === 'unupdated') {
-    return '';
-  }
-  return `Property '${node.name}' was removed`;
 };
 
 const plain = (tree) => {
   const cloneTree = _.cloneDeep(tree);
 
   const iter = (node) => {
-    const result = node.map((unit) => {
+    const valuesPlainDiff = node.map((unit) => {
       const { name } = unit;
 
       if (Object.keys(unit).includes('children')) {
         const { children } = unit;
         const nestedProperties = children.map((child) => {
-          if (Array.isArray(child)) {
-            child.map((grandson) => {
-              // eslint-disable-next-line no-param-reassign
-              grandson.name = `${name}.${grandson.name}`;
-              return grandson;
-            });
-          }
           // eslint-disable-next-line no-param-reassign
           child.name = `${name}.${child.name}`;
           return child;
@@ -55,7 +46,8 @@ const plain = (tree) => {
       return getPlainDiff(unit);
     });
 
-    return _.compact(result).join('\n');
+    const result = _.compact(valuesPlainDiff).join('\n');
+    return result;
   };
 
   return iter(cloneTree);
