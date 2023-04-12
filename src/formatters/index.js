b28e72df86bc84e780fb2d957c4939701b5d8817
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-12 17:09:32 +0300

    upgrade src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 export default (innerTree, format) => {
   switch (format) {
@@ -7,6 +8,8 @@ export default (innerTree, format) => {
       return stylish(innerTree);
     case 'plain':
       return plain(innerTree);
+    case 'json':
+      return json(innerTree);
     default:
       throw new Error(`Формат не поддерживается: ${format}`);
   }
