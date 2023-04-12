08fff03def0888a042dde1015a65d881183dd8e9
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-12 16:01:01 +0300

    upgrade src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,64 +9,70 @@ const getCurrentIndent = (depth, replacer = ' ', spacesCount = 4) => {
   return currentIndent;
 };
 
-const iter = (node, depth, replacer, spacesCount) => {
-  const currentIndent = getCurrentIndent(depth, replacer, spacesCount);
-
-  if (isObject(node)) {
-    const keysAndValues = Object.entries(node);
-    return keysAndValues.map(([key, value]) => {
-      if (isObject(value)) {
-        return `${currentIndent}  ${key}: {\n${iter(value, depth + 1)}${currentIndent}  }\n`;
-      }
-      return `${currentIndent}  ${key}: ${value}\n`;
-    }).join('');
+const getDiffInStatus = (node, currentIndent) => {
+  if (node.status === 'added') {
+    return `${currentIndent}+ ${node.name}: ${node.value}`;
   }
-
-  const result = node.map((unit) => {
-    const { name } = unit;
-
-    if (Object.keys(unit).includes('children')) {
-      const { children } = unit;
-      return `${currentIndent}  ${name}: {\n${iter(children, depth + 1)}\n${currentIndent}  }`;
-    }
-    if (Array.isArray(unit)) {
-      const nearProperties = unit.map((joint) => {
-        if (_.isObject(joint.value) && joint.status === 'removed') {
-          return `${currentIndent}- ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }\n`;
-        }
-        if (_.isObject(joint.value) && joint.status === 'added') {
-          return `${currentIndent}+ ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }`;
-        }
-        if (joint.status === 'removed') {
-          return `${currentIndent}- ${joint.name}: ${joint.value}\n`;
-        }
-        return `${currentIndent}+ ${joint.name}: ${joint.value}`;
-      });
-
-      return nearProperties.join('');
-    }
-
-    if (isObject(unit.value)) {
-      if (unit.status === 'added') {
-        return `${currentIndent}+ ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
-      }
-      if (unit.status === 'removed') {
-        return `${currentIndent}- ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
-      }
-    }
-
-    if (unit.status === 'added') {
-      return `${currentIndent}+ ${name}: ${unit.value}`;
-    }
-    if (unit.status === 'removed') {
-      return `${currentIndent}- ${name}: ${unit.value}`;
-    }
-    return `${currentIndent}  ${name}: ${unit.value}`;
-  });
-
-  return result.join('\n');
+  if (node.status === 'removed') {
+    return `${currentIndent}- ${node.name}: ${node.value}`;
+  }
+  return `${currentIndent}  ${node.name}: ${node.value}`;
 };
 
-const stylish = (tree, depth = 1, replacer = ' ', spacesCount = 4) => `{\n${iter(tree, depth, replacer, spacesCount)}\n}`;
+const stylish = (tree, replacer = ' ', spacesCount = 4) => {
+  const iter = (node, depth) => {
+    const currentIndent = getCurrentIndent(depth, replacer, spacesCount);
+
+    if (isObject(node)) {
+      const keysAndValues = Object.entries(node);
+      return keysAndValues.map(([key, value]) => {
+        if (_.isObject(value)) {
+          return `${currentIndent}  ${key}: {\n${iter(value, depth + 1)}${currentIndent}  }\n`;
+        }
+        return `${currentIndent}  ${key}: ${value}\n`;
+      }).join('');
+    }
+
+    const result = node.map((unit) => {
+      const { name } = unit;
+
+      if (Object.keys(unit).includes('children')) {
+        const { children } = unit;
+        return `${currentIndent}  ${name}: {\n${iter(children, depth + 1)}\n${currentIndent}  }`;
+      }
+      if (Array.isArray(unit)) {
+        const nearProperties = unit.map((joint) => {
+          if (_.isObject(joint.value) && joint.status === 'removed') {
+            return `${currentIndent}- ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }\n`;
+          }
+          if (_.isObject(joint.value) && joint.status === 'added') {
+            return `${currentIndent}+ ${joint.name}: {\n${iter(joint.value, depth + 1)}${currentIndent}  }`;
+          }
+          if (joint.status === 'removed') {
+            return `${currentIndent}- ${joint.name}: ${joint.value}\n`;
+          }
+          return `${currentIndent}+ ${joint.name}: ${joint.value}`;
+        });
+
+        return nearProperties.join('');
+      }
+
+      if (isObject(unit.value)) {
+        if (unit.status === 'added') {
+          return `${currentIndent}+ ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
+        }
+        if (unit.status === 'removed') {
+          return `${currentIndent}- ${name}: {\n${iter(unit.value, depth + 1)}${currentIndent}  }`;
+        }
+      }
+
+      return getDiffInStatus(unit, currentIndent);
+    });
+
+    return result.join('\n');
+  };
+
+  return `{\n${iter(tree, 1)}\n}`;
+};
 
 export default stylish;
