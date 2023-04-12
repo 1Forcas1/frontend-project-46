083de43418f704416441b58d31414694a35059d4
Danil Sichinava <danya.sichinava@mail.ru>
2023-04-12 17:08:42 +0300

    upgrade bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, formatName) => {
+    console.log(genDiff(filepath1, filepath2, formatName.format));
   })
   .parse(process.argv);
